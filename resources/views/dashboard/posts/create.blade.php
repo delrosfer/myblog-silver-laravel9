fca5d3b3cc8e25dabfe93d8831c2f824272b181f
Silverio <plussilver1@gmail.com>
2022-04-01 18:53:17 -0600

    Controlador , ruta y vista post

---
+++ resources/views/dashboard/posts/create.blade.php
@@ -0,0 +1,46 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Post') }}
+        </h2>
+    </x-slot>
+
+    <x-slot name="nav">
+        <div class="space-x-4">
+            {{-- Index --}}
+            <x-jet-nav-link href="{{ route('posts.index') }}" :active="request()->routeIs('posts.index')">
+                {{ __('Index') }}
+            </x-jet-nav-link>
+
+            {{-- Create --}}
+            <x-jet-nav-link href="{{ route('posts.create') }}" :active="request()->routeIs('posts.create')">
+                {{ __('Crear') }}
+            </x-jet-nav-link>
+        </div>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <div class="p-6">
+                    
+                    <form action="{{ route('posts.store') }}" method="POST">
+                        @csrf
+
+                        <div>
+                            <x-jet-label for="name" value="{{ __('Name') }}" />
+                            <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+                            <span class="mt-2 text-xs text-gray-400">Máximo 200 caracteres</span>
+                            <x-jet-input-error for="name" class="mt-2" />
+                        </div>
+
+                        <x-jet-button class="ml-12">
+                            {{ __('Crear') }}
+                        </x-jet-button>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
---
+++ resources/views/dashboard/posts/index.blade.php
@@ -0,0 +1,35 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Posts') }}
+        </h2>
+    </x-slot>
+
+    <x-slot name="nav">
+        <div class="space-x-4">
+            {{-- Index --}}
+            <x-jet-nav-link href="{{ route('posts.index') }}" :active="request()->routeIs('posts.index')">
+                {{ __('Index') }}
+            </x-jet-nav-link>
+
+            {{-- Create --}}
+            <x-jet-nav-link href="{{ route('posts.create') }}" :active="request()->routeIs('posts.create')">
+                {{ __('Crear') }}
+            </x-jet-nav-link>
+        </div>
+    </x-slot>
+
+    <div class="max-w-7xl mt-4 mx-auto sm:px-6 lg:px-8">
+        <x-ui.alerts />
+    </div>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+               
+               Tabla de posts 
+               
+            </div>
+        </div>
+    </div>
+</x-app-layout>
